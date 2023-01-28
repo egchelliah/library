92e878562ce83bcea6247562c18ef7f31c4baed9
ebenezer.chelliah <egchelliah@gmail.com>
2023-01-28 13:45:17 -0700

    Get basic app working

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 let myLibrary = [];
 
 // Constructor
-function Book(name, author, pages, read) {
+function Book(title, author, pages, read) {
 
-    this.name = name
+    this.title = title
     this.author = author
     this.pages = pages
+    this.read = read
 }
 
 function addBookToLibrary() {
@@ -21,7 +22,7 @@ const libraryTable = document.getElementById("libraryTable")
 const addBookButton = document.getElementById("add_book")
 
 
-
+// Add a book
 addBookButton.addEventListener('click', () => {
 
     event.preventDefault()
@@ -34,30 +35,90 @@ addBookButton.addEventListener('click', () => {
 
 })
 
+// Close add book modal
 
 
-submitBook.addEventListener('click', () => {
+function addToArray(){
     event.preventDefault()
     let title = document.getElementById("title").value
     let author = document.getElementById("author").value
     let pages = document.getElementById("pages").value
-    // let read = document.getElementById("read").value
-    alert(title + " " + author + " " + pages)
+    // let read = readValue()
+    let read = document.getElementById("read").checked
+    alert(title + " " + author + " " + pages + " " + read)
 
-    let newBook = new Book(title,author,pages)
+    let newBook = new Book(title,author,pages,read)
     myLibrary.push(newBook)
     console.log(myLibrary)
+    resetForm();
+    loopBooks();
+    // addToTable(newBook)
+}
 
-    let newRow = document.createElement("tr")
-    libraryTable.append(newRow)
+// function addToTable(newBook) {
+//      // Create new table row
+//      let newRow = document.createElement("tr")
+//      libraryTable.append(newRow)
+ 
+//      // Create and add title
+//      let titleCell = document.createElement("td")
+//      titleCell.innerHTML=newBook.title
+//      newRow.append(titleCell)
+ 
+//      // Create and add author
+//      let authorCell = document.createElement("td")
+//      authorCell.innerHTML=newBook.author
+//      newRow.append(authorCell)
+ 
+//      // Create and add author
+//      let pagesCell = document.createElement("td")
+//      pagesCell.innerHTML=newBook.pages
+//      newRow.append(pagesCell)
+// }
 
-    let tr1 = document.createElement("td")
-    tr1.innerHTML=newBook.name
-    newRow.append(tr1)
+function loopBooks(){
+    for (i=(myLibrary.length-1); i<myLibrary.length; i++){
+        let newRow = document.createElement("tr")
+        libraryTable.append(newRow)
+    
+        // Create and add title
+        let titleCell = document.createElement("td")
+        titleCell.innerHTML=myLibrary[i].title
+        newRow.append(titleCell)
+    
+        // Create and add author
+        let authorCell = document.createElement("td")
+        authorCell.innerHTML=myLibrary[i].author
+        newRow.append(authorCell)
+    
+        // Create and add pages
+        let pagesCell = document.createElement("td")
+        pagesCell.innerHTML=myLibrary[i].pages
+        newRow.append(pagesCell)
 
+        // Create and add read status
+        let readCell = document.createElement("td")
+
+        if (myLibrary[i].read === true){
+
+            readCell.innerHTML="Read"
+
+        } else readCell.innerHTML = "Not Read"
+
+        newRow.append(readCell)
+    }
+}
+
+
+function resetForm(){
 
     form.reset()
     form.style.visibility="hidden"
 
+}
+
+// Submit book
+submitBook.addEventListener('click', () => {
+
+    addToArray();
 })
-console.log(myLibrary)
